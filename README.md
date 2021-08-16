fa49e68aa76d8b40bcc839441dc40789976f800d
Kalil Mori Lopes <kalil@MacBook-Pro-de-Hitss.local>
2021-08-16 00:57:10 -0300

    include README

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# terraform
+# Description
+This automation creates a EC2 instance and SG with your IP/32 in ingress rule. The tfstate file is stored on S3
+
+## Create EC2 and SG with your IP
+
+./terraform_apply.sh
+
+## Destroy EC2 and SG with your IP
+
+./terraform_destroy.sh
+
--- tfstate/main.tf
+++ tfstate/main.tf
@@ -2,13 +2,3 @@ provider "aws" {
   region = var.aws_region
   profile = var.aws_profile
 }
-
-# # Variables not allowed here
-# terraform {
-#   backend "s3" {
-#     bucket = "github-tfstate"
-#     key    = "ec2/terraform.state"
-#     region = "us-east-1"
-#     profile = "github-user"
-#   }
-# }
