000b6be4cd85aa85c32aa7ccfc33b60cd8d48ff9
kalildevops <kalil@MacBook-Pro-de-Hitss.local>
2021-09-01 20:52:27 -0300

    Creating basic network resources

---
+++ network/variables.tf
@@ -0,0 +1,48 @@
+# General
+variable aws_region {
+  type = string
+  default = "us-east-1"
+}
+
+variable aws_profile {
+  type = string
+  default = "github-user"
+}
+
+variable tfstate_bucket {
+  type = string
+  default = "github-kalil-tfstate"
+}
+
+variable tfstate_key {
+  type = string
+  default = "network/terraform.state"
+}
+
+# Network
+variable vpc_cidr_block {
+  type        = string
+  default     = "10.0.0.0/20"
+}
+
+variable priv_cidr_block {
+  type        = string
+  default     = "10.0.1.0/25"
+}
+
+variable pub_cidr_block {
+  type        = string
+  default     = "10.0.2.0/25"
+}
+
+variable network_name {
+  type        = string
+  default     = "vpc-github"
+}
+
+variable tags {
+  type = map
+  default = {
+    Creation = "Terraform"
+  }
+}
